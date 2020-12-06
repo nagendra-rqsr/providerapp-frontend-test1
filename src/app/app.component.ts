4cb8190323ce82bdadf44a40cc7c646fdebb1d84
nagendra-rqsr <58209917+nagendra-rqsr@users.noreply.github.com>
2020-12-06 20:48:06 +0530

    design changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,12 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'client-app';
-  manager: any = null;
+  
 
   ngOnInit() {
-    const _mgr = sessionStorage.getItem('MGR_USR');
-    if(_mgr) {
-      this.manager = JSON.parse(_mgr);
-    }
+
   }
 }
--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manager.component.scss']
 })
 export class ManagerComponent implements OnInit {
-
+  manager: any = null;
   constructor() { }
 
   ngOnInit(): void {
+    const _mgr = sessionStorage.getItem('MGR_USR');
+    if(_mgr) {
+      this.manager = JSON.parse(_mgr);
+    }
   }
 
 }
