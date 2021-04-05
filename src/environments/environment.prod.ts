dcc9170678a00abab89e63977758dbd7ecfa46ad
nagendra-rqsr <58209917+nagendra-rqsr@users.noreply.github.com>
2021-04-05 14:53:24 +0530

    update api url with domain name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  appUrl: 'http://13.237.203.199:9001/'
+  appUrl: '	https://providerapp.astutepractice.com.au:9001/'
+  //appUrl: 'http://13.237.203.199:9001/'
   // appUrl: 'http://52.64.1.72:9001/'
 };
